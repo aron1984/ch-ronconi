6e10935d911f108a711eccd0952b59ea16d252ec
Alejandro Ronconi <AARonconi@teco.com.ar>
2022-05-16 01:53:40 -0300

    Cuarta entrega v 1.2 Key, Ternary Operator, parseInt, etc

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -3,12 +3,18 @@ import './ItemCount.css';
 import { Card } from 'react-bootstrap';
 
 // Step 4: add params to father ItemCount and will be avaiable for children functions IMPORTANT
-export default function ItemCount(params) {
+export default function ItemCount(props) {
 
-    // Step 1: add state
-    const [counter, setCounter] = useState(0);
+    // parseInt to props than come as a string
+    const stock = parseInt(props.stock);
+    const initial = parseInt(props.initial);
 
-    // Step 5: create functions decrease and increase
+    // Step 1: add state 
+    const [counter, setCounter] = useState(initial);
+
+
+
+    // Step 5: create functions + and -
     const decrease = () => {
         if (counter > 0) {
             setCounter(counter => counter - 1);
@@ -16,8 +22,8 @@ export default function ItemCount(params) {
     }
 
     const increase = () => {
-        const limit = params.stock;
-        if (counter < limit) {
+        
+        if (counter < stock) {
             setCounter(counter => counter + 1);
         }
     }
@@ -25,11 +31,14 @@ export default function ItemCount(params) {
     // Step 7: create function add to cart
     // I know that the component stateless will have a params called 'product'
     const onAdd = () => {
-
-        if (counter !== 0) {
-            let x = counter;
-            let productName = params.product;
-            console.log('Se agregaron: ', x, ' ', productName)
+        if (stock !== 0){
+            if (counter !== 0) {
+                let x = counter;
+                let productName = props.product;
+                console.log('Se agregaron ', x, ' productos: ', productName)
+            }           
+        }else{
+            console.log('No hay disponible');
         }
     }
 
@@ -37,17 +46,21 @@ export default function ItemCount(params) {
         <div>
             <div style={{ width: '18rem' }} className='containerBtnCount'>
                 <div>
-                    <button variant="secondary" className='btnHand' onClick={() => { decrease() }}>-</button>{/* Step 3: add onClick with corresponding function */}
+                    {/* Step 3: add onClick with corresponding function */}
+                    <button variant="secondary" className='btnHand' onClick={() => { decrease() }}>-</button>
                 </div>
                 <div className='count'>
-                    {counter} {/* Step 2: add counter with {} */}
+                    {/* Step 2: add counter with ternary operator {} */}
+                    {(stock !== 0) ? counter : "Sin Stock"} 
+                    
                 </div>
                 <div>
                     <button className='btnHand' onClick={() => { increase() }}>+</button>
                 </div>
             </div>
             <div>
-                <button className='btnAdd' onClick={() => { onAdd() }}>Add to Cart</button> {/* Step 6: add onClick function with add product to cart */}
+                {/* Step 6: add onClick function with add product to cart */}
+                <button className='btnAdd' onClick={() => { onAdd() }}>Add to Cart</button> 
             </div>
         </div>
     )
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -4,21 +4,23 @@ import ItemCount from './ItemCount/ItemCount';
 import './ItemListContainer.css';
 
 export default function ItemListContainer() {
-  // aca puede ir una logica de iteración
-
-  let product = ['Zapatillas','Remeras','Conjunto','Medias'] ;
+  
+  // let product = ['Zapatillas','Remeras','Conjunto','Medias'] ;
+  let product = [{id:1, name:'Zapatillas', stock: 10},{id:2, name:'Remeras', stock: 5},{id:3, name:'Conjunto', stock:7},{id:4, name:'Medias', stock:0}] ;
   
   // arrow function ???
   // no se si está bien el return, si lo saco no funciona
   const productList =  product.map((product) => {
-    // En esta lína tengo dudas, pero es la forma que no me da error. ¿Está bien el 'retur'?
+
+
+    // En esta lína tengo dudas: es la forma que no me da error.
     return (  
-      <Card className='mx-2'>
+      <Card className='mx-2' key={product.id.toString()}>
         <div className='cardImage'></div>
         <Card.Body className="cardEdit">
 
           {/*Step 8:  add name prodcut from params App.js  */}
-          <Card.Title>{product}</Card.Title>
+          <Card.Title>{product.name}</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the bulk of
             the card's content.
@@ -27,8 +29,8 @@ export default function ItemListContainer() {
 
         {/* After work with ItemCount.jsx, edit here:
             Step 1: Add ItemCount component container */}
-        {/* Step 2: Add params 'product={product}' to show at console 'what product is adding' */}
-        <ItemCount product={product} stock="5" initial="1" />
+        {/* Step 2: Add props 'product={product}' to show at console 'what product is adding' */}
+        <ItemCount product={product.name} stock={product.stock} initial="1" />
       </Card> 
      )
     }
